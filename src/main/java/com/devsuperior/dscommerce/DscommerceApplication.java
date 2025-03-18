6898e7a8dd67f3d609d6c9cd843ddd5a6123158e
saulopietro <saulo154@outlook.com>
2025-03-18 08:26:03 -0300

    Domain model User Role, seed

---
+++ src/main/java/com/devsuperior/dscommerce/DscommerceApplication.java
@@ -0,0 +1,15 @@
+package com.devsuperior.dscommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Service;
+
+@Service
+@SpringBootApplication
+public class DscommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscommerceApplication.class, args);
+	}
+
+}

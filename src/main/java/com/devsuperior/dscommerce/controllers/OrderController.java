5cc0dd77aa3aad8804ef0fc7e2145ba86860090c
saulopietro <saulo154@outlook.com>
2025-03-20 12:34:38 -0300

    Saving Order

--- src/main/java/com/devsuperior/dscommerce/controllers/OrderController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/OrderController.java
@@ -30,4 +30,12 @@ public class OrderController {
         return ResponseEntity.ok(dto);
     }
 
-}
+    @PreAuthorize("hasRole('ROLE_CLIENT')")
+    @PostMapping
+    public ResponseEntity<OrderDTO> insert(@Valid @RequestBody OrderDTO dto) {
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+
+}}
